46dbc190b5a5c5c8ade2e02403bc04a9df4168fb
xaviermasle <xmas@full-on-net.com>
2021-11-24 21:21:53 +0100

    Skeleton

--- schema_registry/avro/register_avro_schema.py
+++ schema_registry/avro/register_avro_schema.py
@@ -1,20 +1,18 @@
+from logging import Logger
+
 from confluent_kafka.schema_registry import SchemaRegistryClient, Schema
 
+from logger.logger import get_logger
+
+logger: Logger = get_logger()
 sr_conf: dict = {'url': "http://localhost:8081"}
 schema_registry_client: SchemaRegistryClient = SchemaRegistryClient(sr_conf)
 
-schema_str: str = '''
-{
-"namespace": "com.telefonica",
- "type": "record",
- "name": "Subject",
- "fields": [
-     {"name": "name", "type": "string"}
- ]
-}
-'''
+file_object = open("person.avsc", "r")
+schema_str: str = file_object.read()
+file_object.close()
 schema: Schema = Schema(schema_str=schema_str, schema_type="AVRO")
 
-subject: str = "my-subject"
+subject: str = "person"
 schema_id: int = schema_registry_client.register_schema(subject_name=subject, schema=schema)
-print(f'Schema deployed {schema_id}')
+logger.debug(f'Schema deployed with id: {schema_id} and subject: {subject}')
